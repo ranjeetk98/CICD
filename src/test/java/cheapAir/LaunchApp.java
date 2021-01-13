2845be8a39254945eb3ab75b320fe33035664200
Ranjeet Kumar <Ranjeet.Kumar@elliemae.com>
2021-01-13 17:01:51 +0530

    Jenkins job trigger

--- src/test/java/cheapAir/LaunchApp.java
+++ src/test/java/cheapAir/LaunchApp.java
@@ -13,7 +13,7 @@ public class LaunchApp {
 		public void launchUrl() throws InterruptedException {
 
 			
-//	-		System.setProperty("webdriver.chrome.driver", "D:\\Users\\rkumar4\\Workspace\\CICD_selenium\\resources\\chromedriver.exe");
+//  		System.setProperty("webdriver.chrome.driver", "D:\\Users\\rkumar4\\Workspace\\CICD_selenium\\resources\\chromedriver.exe");
 			System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
 			
 			WebDriver driver = new ChromeDriver();			
@@ -24,6 +24,7 @@ public class LaunchApp {
 			
 			driver.close();
 			driver.quit();
+//			Jenkins job trigger
 			
 		}
 
