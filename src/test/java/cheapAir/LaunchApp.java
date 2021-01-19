ba1f4a6041d4b1f2c4538b08ad922fe39c13ab61
ranjeetk98 <ranjeetk98@gmail.com>
2021-01-19 17:42:32 +0530

    selenium grid on docker

--- src/test/java/cheapAir/LaunchApp.java
+++ src/test/java/cheapAir/LaunchApp.java
@@ -1,24 +1,27 @@
 package cheapAir;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class LaunchApp {
 	
 		
 		@Test
-		public void launchUrl() throws InterruptedException {
+		public void launchUrl() throws InterruptedException, MalformedURLException {
 
 
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setBrowserName(BrowserType.CHROME);	
 			
-//  		System.setProperty("webdriver.chrome.driver", "D:\\Users\\rkumar4\\Workspace\\CICD_selenium\\resources\\chromedriver.exe");
-
-			System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+			WebDriver driver = new RemoteWebDriver(new URL("http://3.129.13.97:4545/wd/hub"),cap);			
 			
-			WebDriver driver = new ChromeDriver();			
 			driver.get("https://www.cheapair.com/");			
 			driver.manage().window().maximize();			
 			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
